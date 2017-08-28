b3038ba6be9d18902697c145fc0cad096de05c48
Andres <ballerlikemike23@gmail.com>
2017-08-28 01:40:41 -0400

    more tests for respec

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,30 @@
 class PostsController < ApplicationController
 
-	def index
-	end
+  def index
+  end
 
-	def new
-		@post = Post.new
-	end
+  def new
+    @post = Post.new
+  end
 
-	def create
-		@post = Post.create(post_params)
-		redirect_to @post
-	end
+  def create
+    @post = Post.create(post_params)
+    if @post.save
+      flash[:success] = "Your post has been created."
+      redirect_to @post
+    else
+      flash[:alert] = "Halt, you fiend! You need an image to post here!"
+      render :new
+    end
+  end
 
-	def show
-		@post = Post.find(params[:id])
-	end
+  def show
+    @post = Post.find(params[:id])
+  end
 
-	private
+  private
 
-	def post_params
-		params.require(:post).permit(:image, :caption)
-	end
+  def post_params
+    params.require(:post).permit(:image, :caption)
+  end
 end
